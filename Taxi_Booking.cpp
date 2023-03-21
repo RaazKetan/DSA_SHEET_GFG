8c35710a0b3a90ff9f419d41f9ab2aaa102c9880
Raaz <21ketanraaz@gmail.com>
2023-03-21 20:47:38 +0530

    +Easy

---
+++ Taxi_Booking.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int minimumTime(int N,int cur,vector<int> &pos,vector<int> &time){
+      
+           int ans = INT_MAX;
+           int temp1 =0;
+        for(int i = 1; i < pos.size(); i++)
+        {
+            temp1 = abs(cur-pos[i])*time[i];
+            ans = min(ans, temp1);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N,cur;
+        cin>>N>>cur;
+        vector<int> pos(N),time(N);
+        for(int i=0;i<N;i++){
+            cin>>pos[i];
+        }
+        for(int i=0;i<N;i++){
+            cin>>time[i];
+        }
+        Solution ob;
+        cout<<ob.minimumTime(N,cur,pos,time)<<endl;
+    }
+}
+// } Driver Code Ends
