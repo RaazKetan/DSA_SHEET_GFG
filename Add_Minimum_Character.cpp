bd6e998c288343034211555521833f6b6a6e29e2
Raaz <21ketanraaz@gmail.com>
2023-04-07 09:49:34 +0530

    PTOD medium

---
+++ Add_Minimum_Character.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int addMinChar(string str){
+        int i = 0, j = str.length()-1;
+        int count = 0;
+        while(i<=j)
+        {
+            if(str[i] != str[j])
+            {
+                count++;
+                i = 0;
+                j = str.length()-1-count;
+            }
+            else{
+            i++;
+            j--;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        
+        Solution ob;
+        cout << ob.addMinChar(str) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
